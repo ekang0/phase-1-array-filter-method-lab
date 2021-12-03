ea1cdfc8e0312fb293f25b5d56d4f9c9208442d1
Esther Kang <eskang87@gmail.com>
2021-12-02 17:52:21 -0800

    commit tree 420c5eb1e58567cdd3bf728658409f01c45ecdbe

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, stringArg){
+    let drivers = array.filter(element => element === stringArg || element.toUpperCase() === stringArg.toUpperCase());
+    return drivers;
+};
+
+function fuzzyMatch(array, matchLetters) {
+    let length = matchLetters.length;
+    let match = array.filter(element => element.slice(0, length) === matchLetters);
+    return match;
+};
+
+function matchName(array, name){
+    const objMatchName = array.filter(element => element.name === name);
+    return objMatchName;
+};
